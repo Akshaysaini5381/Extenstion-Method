31ba489957aef0cf76c11b5f5531c1a7904dbde1
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-07-31 18:26:22 +0530

    Add project files.

---
+++ Extenstion Method/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Extenstion_Method
+{
+    static class a 
+    {
+     public static void M(this Program d)
+        {
+            Console.WriteLine("saini :");
+        }
+
+        public static void M1(this Program d)
+        {
+            Console.WriteLine("ashi : :");
+        }
+
+    }
+
+    
+    class Program
+    {
+
+      /*  void m()
+        {
+            Console.WriteLine("akshay :");
+        }*/
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            //  obj.m();
+            obj.M();
+            obj.M1();
+        }
+    }
+}
